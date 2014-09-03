a8919e885ae4ca1919f444a91f6a0a1b89bd4fa9
Birk Marcus <birkmarcus@gmail.com>
2014-09-03 15:55:58 +0200

    !!BIG CHANGE !!  JS height implementation, pictures=background

--- animate.js
+++ animate.js
@@ -34,74 +34,31 @@ $(document).ready(function () {
         $("#knapVenstre").removeClass('activeKnap');
     });
 
-// $("#knapBund").click(function () {
-//         if($('#knapBund').hasClass('activeKnap')) {
-//             $('#knapBund').removeClass('activeKnap');   
-//         } else {
-//             $('#knapBund').addClass('activeKnap');
-//         }
-//     });
 
-// $("#knapVenstre").click(function () {file:///Users/Bruger/Dropbox/Older%20v2/Js-fiddle/index.html
-//         if($('#knapVenstre').hasClass('activeKnap')) {
-//             $('#knapVenstre').removeClass('activeKnap');   
-//         } else {
-//             $('#knapVenstre').addClass('activeKnap');
-//         }
-//     });
-
-// $("#knapHojre").click(function () {
-//         if($('#knapHojre').hasClass('activeKnap')) {
-//             $('#knapHojre').removeClass('activeKnap');   
-//         } else {
-//             $('#knapHojre').addClass('activeKnap');
-//         }
-//     });
-
-
-    //BXSlider
 $('.bxslider').bxSlider({});
+
+
+
+
+
+
+  function setHeight() {
+    windowHeight = $(window).innerHeight();
+    $('.sliderImage').css('background-size', 'auto '+windowHeight / 2+'px');
+  };
+  setHeight();
+  
+  $(window).resize(function() {
+    setHeight();
+  });
 });
 
 
+//height
 
-
-
-
-  
-
-//Z-index
-
-  maxZ = $('div:last').css("zIndex");
-$("#knapTop").click(function()
-    {
-        maxZ++;
-      $('#BusyBox1').css('z-index',maxZ);           
-    }
-);
-  
-
-   maxZ = $('div:last').css("zIndex");
-$("#knapBund").click(function()
-    {
-        maxZ++;
-      $('#BusyBox2').css('z-index',maxZ);           
-    }
-);  
-
-     maxZ = $('div:last').css("zIndex");
-$("#knapVenstre").click(function()
-    {
-        maxZ++;
-      $('#BusyBox3').css('z-index',maxZ);           
-    }
-);
-    maxZ = $('div:last').css("zIndex");
-$("#knapHojre").click(function()
-    {
-        maxZ++;
-      $('#BusyBox4').css('z-index',maxZ);           
-    }
-);
+// function autoHeight() {
+//     var winHeight = $(window).height();
+//     $(".sliderImage").css({'height':varHeight});
+// }
 
 
